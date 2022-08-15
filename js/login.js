56636f2ed3366da95fdf9d26e0450ab75497530b
Oliulla <razasm29@gmail.com>
2022-08-16 01:38:57 +0600

    changed login form

--- js/login.js
+++ js/login.js
@@ -8,6 +8,12 @@ document.getElementById('btn-submit').addEventListener('click', () => {
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
 
+    if (typeof(userEmail) === 'string' && typeof(userPassword) === 'string') {
+        window.location.href = 'bank.html';
+    } else {
+        alert('Wrong email or password');
+    }
+
     //Danger: DO NOT VERIFY email password on the client side
     // step-4: verify email and password and check user either valid or Invalid
     // if(userEmail === 'sontan@baap.com' && userPassword === 'secret'){
